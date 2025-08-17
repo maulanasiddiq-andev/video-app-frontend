463002f397ce23494afbad6db6c2b2e9927e2275
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-17 17:15:42 +0700

    create: login feature

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { BaseResponse } from '../models/base-response';
+import { Token } from '../models/token';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  url: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   public get isAuthenticated(): boolean {
     const token = localStorage.getItem("token");
@@ -13,5 +21,9 @@ export class AuthService {
       return false;
     }
     return true;
+  }
+
+  public login(body: any): Observable<BaseResponse<Token>> {
+    return this.http.post(this.url + "login", body).pipe(map((response: BaseResponse<Token>) => response));
   }
 }
--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,19 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   goToRegister(event: Event) {
     event.preventDefault();
     this.router.navigate(['/auth/register'], { replaceUrl: true })
+  }
+
+  login() {
+    this.authService.login(this.loginForm.value).subscribe(result => {
+      if (result.succeed) {
+        localStorage.setItem('token', result.data.token);
+
+        const jsonUser = JSON.stringify(result.data.user);
+        localStorage.setItem('user', jsonUser);
+        
+        this.router.navigate([''], { replaceUrl: true });
+      }
+    });
   }
 }
