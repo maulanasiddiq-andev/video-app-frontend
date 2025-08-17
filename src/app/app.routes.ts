44a9703f9c227ab69552e83db98f8cb4888fc666
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-18 04:27:37 +0700

    create: layout for video pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { authGuard } from './core/guards/auth.guard';
 import { unauthGuard } from './core/guards/unauth.guard';
+import { VideoLayoutComponent } from './layouts/video-layout/video-layout.component';
 
 export const routes: Routes = [
     {
         path: '',
         canActivate: [authGuard],
+        component: VideoLayoutComponent,
         loadChildren: () => import('./modules/videos/videos.module').then(mod => mod.VideosModule)
     },
     {
---
+++ src/app/layouts/video-layout/video-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { VideosRoutingModule } from 'src/app/modules/videos/videos-routing.module';
+
+@Component({
+  selector: 'app-video-layout',
+  imports: [VideosRoutingModule],
+  templateUrl: './video-layout.component.html',
+  styleUrl: './video-layout.component.scss'
+})
+export class VideoLayoutComponent {
+
+}
